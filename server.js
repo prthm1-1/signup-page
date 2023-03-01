6637df613555807521f6b6adb132e157e1798ff4
Pratham Pandey <pratham1.pp@gmail.com>
2023-03-01 20:34:31 +0530

    promise commit

--- server.js
+++ server.js
@@ -37,15 +37,30 @@ app.post("/",function(req,res){
     }
     const request=https.request(url,options,function(response){
         
-        if(response.statusCode===200){
-            res.sendFile(__dirname+"/success.html");
-        }
-        else{
-            res.sendFile(__dirname+"/failure.html");
-        }
+        // if(response.statusCode===200){
+        //     res.sendFile(__dirname+"/success.html");
+        // }
+        // else{
+        //     res.sendFile(__dirname+"/failure.html");
+        // }
         
+        const displayFile=new Promise((resolve,reject)=>{
+            if(response.statusCode===200){
+                    resolve(__dirname+"/success.html");
+                }else{
+                    rejects(__dirname+"/failure.html");
+                }
+        })
+        displayFile.then((v)=>{
+        res.sendFile(v);
+        }).catch((e)=>{
+        res.sendFile(e);
+        });
+        
+
+
         response.on("data",function(data){
-            console.log(data);
+            console.log(JSON.parse(data));
         })
     })
     request.write(jsonData);
@@ -53,6 +68,10 @@ app.post("/",function(req,res){
 })
 
 app.post("/failure",function(req,res){
+    res.redirect("/");
+})
+
+app.post("/success",function(req,res){
     res.redirect("/");
 })
 
